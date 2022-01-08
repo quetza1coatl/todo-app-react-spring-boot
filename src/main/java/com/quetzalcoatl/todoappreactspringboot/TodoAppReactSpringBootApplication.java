7b396d13d7e2f4a0691d16d2b86ef605f1c6c722
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2022-01-08 16:17:27 +0300

    init + H2 config

---
+++ src/main/java/com/quetzalcoatl/todoappreactspringboot/TodoAppReactSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.quetzalcoatl.todoappreactspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppReactSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppReactSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quetzalcoatl/todoappreactspringboot/TodoAppReactSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quetzalcoatl.todoappreactspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppReactSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
